30b7c070d7515f748cabb1e2b47e15d2905ae69e
Barni S <barni@google.com>
2020-09-18 19:46:31 -0400

    add reference to gcp api for catalog producer

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.14
 require (
 	github.com/GoogleContainerTools/kpt v0.33.0
 	github.com/stretchr/testify v1.4.0
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	google.golang.org/api v0.4.0
 	sigs.k8s.io/kustomize/kyaml v0.7.2-0.20200914180048-6a0a909e7315
 )
 
--- internal/gcp/product/product_api.go
+++ internal/gcp/product/product_api.go
@@ -4,8 +4,12 @@
 package product
 
 import (
+	"context"
 	"fmt"
+
 	"github.com/pcingest/api/catalog"
+	"golang.org/x/oauth2/google"
+	cpcp "google.golang.org/api/cloudprivatecatalogproducer/v1beta1"
 )
 
 func Reconcile(p catalog.Product) error {
@@ -16,6 +20,20 @@ func Reconcile(p catalog.Product) error {
 	// If does not exist : create new one
 	// If if does exist: update conditionally
 
+	// Use oauth2.NoContext if there isn't a good context to pass in.
+	ctx := context.Background()
+
+	client, err := google.DefaultClient(ctx, cpcp.CloudPlatformScope)
+	if err != nil {
+		return err
+	}
+	_, err = cpcp.New(client)
+	if err != nil {
+		return err
+	}
+
+	// Reference:
+	// https://github.com/googleapis/google-api-go-client/blob/master/cloudprivatecatalogproducer/v1beta1/cloudprivatecatalogproducer-gen.go
 	fmt.Printf("Reconciling Product: \n%s\n", p)
 	if true {
 		//panic("Missing API integration")
